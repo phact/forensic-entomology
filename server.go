7b70414c48a8b706a7ffaed480c85f1e8a1f074e
Sebastian Estevez <sebastianestevez@Sebastians-MBP.fios-router.home>
2016-01-15 13:39:39 -0500

    adding dtcs.html and json

--- server.go
+++ server.go
@@ -65,6 +65,7 @@ func ServeHTTP() {
 	var args string
 	var args22 string
 	var args3 string
+	var argsDTCS string
 
 	args = "?jql=project+%3D+CASSANDRA+AND+fixVersion+in+%282.1.4%2C+%222.1+beta1%22%2C+%222.1+beta2%22%2C+%222.1+rc1%22%2C+%222.1+rc2%22%2C+%222.1+rc3%22%2C+%222.1+rc4%22%2C+%222.1+rc5%22%2C+%222.1+rc6%22%2C+2.1.0%2C+2.1.1%2C+2.1.2%2C+2.1.3%29+ORDER+BY+watchers+DESC&maxResults=100"
 
@@ -73,13 +74,16 @@ func ServeHTTP() {
 
 	args22 = "?jql=project+%3D+CASSANDRA+AND+fixVersion+in+(2.2.0,'2.2.0%20beta%201',2.2.x,'2.2.0%20rc1','2.2.0%20rc2',2.2.0,2.2.1)+ORDER+BY+watchers+DESC&maxResults=100"
 	args3 = "?jql=project+%3D+CASSANDRA+AND+fixVersion+in+(3.0.x,'3.0%20beta%201','3.0.0%20rc1')+ORDER+BY+watchers+DESC&maxResults=100"
+	argsDTCS = "?jql=project+%3D+CASSANDRA+AND+text~DTCS+ORDER+BY+watchers+DESC&maxResults=100"
 	hIssues22 := makeRequest("https://issues.apache.org/jira/rest/api/2/search", args22)
 	hIssues3 := makeRequest("https://issues.apache.org/jira/rest/api/2/search", args3)
+	hIssuesDTCS := makeRequest("https://issues.apache.org/jira/rest/api/2/search", argsDTCS)
 
 	http.Handle("/", http.FileServer(http.Dir("./www/")))
 	http.Handle("/jira.json", http.StripPrefix("/jira.json", hIssues))
 	http.Handle("/jira22.json", http.StripPrefix("/jira22.json", hIssues22))
 	http.Handle("/jira3.json", http.StripPrefix("/jira3.json", hIssues3))
+	http.Handle("/dtcs.json", http.StripPrefix("/dtcs.json", hIssuesDTCS))
 	http.Handle("/projects.json", hProjects)
 	http.Handle("/versions.json", hVersions)
 
